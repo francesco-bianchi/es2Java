a9ed66b075e4f520a0686f28cb1793396cbfb687
lai2-24 <lai2-24@itismeucci.com>
2024-03-22 13:23:50 +0100

    Finale

--- es2/src/main/java/es2/arr/Main.java
+++ es2/src/main/java/es2/arr/Main.java
@@ -2,9 +2,25 @@ package es2.arr;
 
 public class Main {
     public static void main(String[] args) {
+        ThreadCas[] arrayThread = new ThreadCas[10];
         int[] arr = new int[10];
-        for(int i=0; i<arr.length; i++){
-            
+        for(int i=0; i<arr.length;i++){
+            arrayThread[i] = new ThreadCas(arr, i);
         }
+        for(int i=0; i<arr.length;i++){
+            arrayThread[i].start();
+        }
+        for(int i=0; i<arr.length;i++){
+            try {
+                arrayThread[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
     }
 }
--- es2/src/main/java/es2/arr/ThreadCas.java
+++ es2/src/main/java/es2/arr/ThreadCas.java
@@ -1,5 +1,18 @@
 package es2.arr;
 
 public class ThreadCas extends Thread{
-    
+    int indice;
+    int[] array = new int[10];
+
+    ThreadCas(int[] array, int indice) {
+        this.indice = indice;
+        this.array = array;
+    }
+
+    @Override
+    public void run() {
+        int val = (int) (Math.random() * 100)+1;
+        array[indice] = val;
+        System.out.println(Thread.currentThread().getName() + " ha messo " + val);
+    }
 }
